33da2a2d602c75e6d3283aeb229548cbe068bae1
Aaron Schwartz <aschwartz628@gmail.com>
2017-02-25 15:28:37 -0500

    added contract model and tested database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var server = express();
-var userRouter = require('./server/routers/user.router.js');
+var contractRouter = require('./server/routers/contract.router.js');
 var mongoose = require('mongoose');
 var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
 mongoose.connect(mongoURI);
@@ -12,7 +12,7 @@ server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
 
-server.use(userRouter);
+server.use(contractRouter);
 
 server.listen(8080, function(){
   console.log("Now listening on port...", 8080);
